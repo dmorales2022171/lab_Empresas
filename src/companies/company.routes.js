91f3043cb75f392067a05a0f150b688a859325df
dmorales2022171 <dmorales-2022171@kinal.edu.gt>
2024-03-03 15:46:41 -0600

    se termino el modulo company

--- src/companies/company.routes.js
+++ src/companies/company.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { check } from "express-validator";
-import { companyGet, companyPost, companyPut } from "./company.controller.js";
-import {validateFilds} from '../middlewares/validate-filds.js'
-import {validateJWT} from '../middlewares/validate-jwt.js'
-import {hasRoleAuthorized} from '../middlewares/validate.role.js'
+import { companyGet, companyPost, companyPut, generateCompanyReport, getCompanyByAcendent, getCompanyByCategory, getCompanyByDecendent, getCompanyByYearsTrayectory } from "./company.controller.js";
+import { validateFilds } from '../middlewares/validate-filds.js'
+import { validateJWT } from '../middlewares/validate-jwt.js'
+import { hasRoleAuthorized } from '../middlewares/validate.role.js'
 import { existsCompany } from "../helpers/db-validators.js";
 
 const router = Router();
@@ -28,8 +28,8 @@ router.put(
         validateJWT,
         hasRoleAuthorized('ADMIN_ROLE'),
         check("id", "it is not valid id").isMongoId(),
-        check("id").custom(existsCompany),  
-        validateFilds      
+        check("id").custom(existsCompany),
+        validateFilds
     ],
     companyPut
 )
@@ -41,6 +41,47 @@ router.get(
         hasRoleAuthorized('ADMIN_ROLE'),
     ],
     companyGet
+)
+
+router.get(
+    '/yearsTrayectory/:yearsTrayectory',
+    getCompanyByYearsTrayectory
+)
+
+router.get(
+    '/category/:category',
+    [
+        validateJWT,
+        hasRoleAuthorized('ADMIN_ROLE'),
+    ],
+    getCompanyByCategory
+)
+
+router.get(
+    '/A_Z',
+    [
+        validateJWT,
+        hasRoleAuthorized('ADMIN_ROLE'),
+    ],
+    getCompanyByAcendent
+)
+
+router.get(
+    '/Z_A',
+    [
+        validateJWT,
+        hasRoleAuthorized('ADMIN_ROLE'),
+    ],
+    getCompanyByDecendent
+)
+
+router.get(
+    '/report',
+    [
+        validateJWT,
+        hasRoleAuthorized('ADMIN_ROLE'),
+    ],
+    generateCompanyReport
 )
 
 export default router;
